5b6fcf144d55f442ef69bc86946cce3d2cd55fb2
tamaco489 <tamaco489@gmail.com>
2025-07-08 05:55:00 +0900

    add: aws configuration

---
+++ batch/shop_event_projector/internal/configuration/configuration.go
@@ -0,0 +1,51 @@
+package configuration
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/kelseyhightower/envconfig"
+)
+
+var globalConfig Config
+
+type Config struct {
+	AWSConfig   aws.Config
+	Env         string `envconfig:"ENV" default:"dev"`
+	ServiceName string `envconfig:"SERVICE_NAME" default:"shop-event-projector"`
+
+	CoreDB struct {
+		Host string `json:"host"`
+		Port string `json:"port"`
+		User string `json:"username"`
+		Pass string `json:"password"`
+		Name string `json:"dbname"`
+	}
+}
+
+func Get() Config { return globalConfig }
+
+func Load(ctx context.Context) (Config, error) {
+	envconfig.MustProcess("", &globalConfig)
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	env := globalConfig.Env
+	if err := loadAWSConf(ctx, env); err != nil {
+		return globalConfig, err
+	}
+
+	if err := batchGetSecrets(
+		ctx,
+		globalConfig.AWSConfig,
+		map[string]any{
+			fmt.Sprintf("core/%s/rds-cluster", env): &globalConfig.CoreDB,
+		},
+	); err != nil {
+		return Config{}, err
+	}
+
+	return globalConfig, nil
+}
