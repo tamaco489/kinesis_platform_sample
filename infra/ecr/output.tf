2ad695ebc3f5efd519e94e30b2b5bf79b6dcf953
tamaco489 <tamaco489@gmail.com>
2025-07-08 07:02:32 +0900

    feature/shop event projector ecr (#2)
    * add: base.mk
    
    * add: ecr
    
    * rename: shop event projector docker file
    
    * add: asdf .tool-versions
    
    * add: shop event projector's Makefile

---
+++ infra/ecr/output.tf
@@ -0,0 +1,8 @@
+output "shop_event_projector" {
+  value = {
+    arn  = aws_ecr_repository.shop_event_projector.arn
+    id   = aws_ecr_repository.shop_event_projector.id
+    name = aws_ecr_repository.shop_event_projector.name
+    url  = aws_ecr_repository.shop_event_projector.repository_url
+  }
+}
---
+++ infra/ecr/variables.tf
@@ -0,0 +1,16 @@
+variable "env" {
+  description = "The environment in which the ecr will be created"
+  type        = string
+  default     = "stg"
+}
+
+variable "project" {
+  description = "The project name"
+  type        = string
+  default     = "kinesis-platform-sample"
+}
+
+locals {
+  fqn      = "${var.env}-${var.project}"
+  shop_event_projector = "${var.env}-shop-event-projector"
+}
