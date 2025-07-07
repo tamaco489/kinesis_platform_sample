5069cadcff54dbb69b06215349f361a0ff68f663
tamaco489 <tamaco489@gmail.com>
2025-07-08 07:08:46 +0900

    add: network (#3)

---
+++ infra/network/provider.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  default_tags {
+    tags = {
+      Project = var.project
+      Env     = var.env
+      Managed = "terraform"
+    }
+  }
+}
+
+terraform {
+  required_version = "1.12.2"
+  backend "s3" {
+    bucket = "stg-kinesis-platform-sample-tfstate"
+    key    = "network/terraform.tfstate"
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+}
