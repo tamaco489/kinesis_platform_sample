251d8da979581ea0b10e0b861deba3b3a6a57f54
tamaco489 <tamaco489@gmail.com>
2025-07-09 03:06:48 +0900

    fix: use lambda event source mapping on shop event projector

--- infra/service/batch/shop_event_projector/data.tf
+++ infra/service/batch/shop_event_projector/data.tf
@@ -14,14 +14,13 @@ data "terraform_remote_state" "network" {
   }
 }
 
-# todo: Enable after creating KDS
-# data "terraform_remote_state" "kinesis" {
-#   backend = "s3"
-#   config = {
-#     bucket = "${var.env}-kinesis-platform-sample-tfstate"
-#     key    = "kinesis/terraform.tfstate"
-#   }
-# }
+data "terraform_remote_state" "kinesis" {
+  backend = "s3"
+  config = {
+    bucket = "${var.env}-kinesis-platform-sample-tfstate"
+    key    = "kinesis/terraform.tfstate"
+  }
+}
 
 # AWS managed key
 data "aws_kms_key" "secretsmanager" {
