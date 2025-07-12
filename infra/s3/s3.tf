39d14d905771065c151a2da585459c54e4db04f0
tamaco489 <tamaco489@gmail.com>
2025-07-12 14:29:43 +0900

    feature/via kinesis data firehose logs (#6)
    * add: s3 resource
    
    * add: lifecycle policy rule

---
+++ infra/s3/s3.tf
@@ -0,0 +1,74 @@
+# s3 bucket for various logs (event, audit, error) via kinesis data firehose
+# directory structure: {application}/{log_type}/{year}/{month}/{day}/
+# example: shop_event_projector/event/2025/01/01/
+resource "aws_s3_bucket" "logs" {
+  bucket = "${local.fqn}-logs"
+
+  tags = { Name = "${local.fqn}-logs" }
+}
+
+# enable versioning for s3 bucket
+resource "aws_s3_bucket_versioning" "logs" {
+  bucket = aws_s3_bucket.logs.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# enable server side encryption for s3 bucket
+resource "aws_s3_bucket_server_side_encryption_configuration" "logs" {
+  bucket = aws_s3_bucket.logs.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# enable public access block for s3 bucket
+resource "aws_s3_bucket_public_access_block" "logs" {
+  bucket = aws_s3_bucket.logs.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# allow write access from kinesis data firehose
+resource "aws_s3_bucket_policy" "logs" {
+  bucket = aws_s3_bucket.logs.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowKinesisFirehoseServicePrincipal"
+        Effect = "Allow"
+        Principal = {
+          Service = "firehose.amazonaws.com"
+        }
+        Action = [
+          "s3:AbortMultipartUpload",
+          "s3:GetBucketLocation",
+          "s3:GetObject",
+          "s3:ListBucket",
+          "s3:ListBucketMultipartUploads",
+          "s3:PutObject"
+        ]
+        Resource = [
+          aws_s3_bucket.logs.arn,
+          "${aws_s3_bucket.logs.arn}/*"
+        ]
+        Condition = {
+          StringEquals = {
+            "aws:SourceAccount" = data.aws_caller_identity.current.account_id
+          }
+        }
+      }
+    ]
+  })
+}
+
+data "aws_caller_identity" "current" {}
