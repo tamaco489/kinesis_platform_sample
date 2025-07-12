1c2719a0ae7eeeca2551173aa261117f3ce4e822
tamaco489 <tamaco489@gmail.com>
2025-07-12 22:11:19 +0900

    add: secret manager endpoint

--- infra/service/batch/shop_event_projector/security_group.tf
+++ infra/service/batch/shop_event_projector/security_group.tf
@@ -4,9 +4,31 @@ resource "aws_security_group" "shop_event_projector" {
   vpc_id      = data.terraform_remote_state.network.outputs.vpc.id
 }
 
-resource "aws_vpc_security_group_egress_rule" "shop_event_projector_egress" {
-  security_group_id = aws_security_group.shop_event_projector.id
-  description       = "allow Lambda to access external resources (e.g. rds, dynamodb, api)"
-  ip_protocol       = "-1"
-  cidr_ipv4         = "0.0.0.0/0"
+# Security group for connecting to Secrets Manager VPC endpoint
+resource "aws_security_group" "secrets_manager_endpoint" {
+  name        = "${local.fqn}-secrets-manager-endpoint"
+  description = "Security group for Secrets Manager endpoint"
+  vpc_id      = data.terraform_remote_state.network.outputs.vpc.id
+
+  tags = { Name = local.fqn }
+}
+
+# Shop Event Projector SG -> Secrets Manager endpoint SG (egress)
+resource "aws_vpc_security_group_egress_rule" "shop_event_projector_to_secrets_manager_endpoint" {
+  security_group_id            = aws_security_group.shop_event_projector.id
+  description                  = "Allow HTTPS to Secrets Manager endpoint SG"
+  from_port                    = 443
+  to_port                      = 443
+  ip_protocol                  = "tcp"
+  referenced_security_group_id = aws_security_group.secrets_manager_endpoint.id
+}
+
+# Lambda Shop Event Projector <- secrets_manager_endpoint (ingress)
+resource "aws_vpc_security_group_ingress_rule" "secrets_manager_endpoint_from_shop_event_projector" {
+  security_group_id            = aws_security_group.secrets_manager_endpoint.id
+  description                  = "Allow HTTPS from Shop Event Projector SG"
+  from_port                    = 443
+  to_port                      = 443
+  ip_protocol                  = "tcp"
+  referenced_security_group_id = aws_security_group.shop_event_projector.id
 }
