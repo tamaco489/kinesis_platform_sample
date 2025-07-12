78a345c06ef0fa6049fd46529e19b537b5650044
tamaco489 <tamaco489@gmail.com>
2025-07-12 23:20:34 +0900

    add: shop api service ecr

--- infra/ecr/output.tf
+++ infra/ecr/output.tf
@@ -1,3 +1,13 @@
+output "shop_api" {
+  description = "ecr repository for shop api"
+  value = {
+    arn  = aws_ecr_repository.shop_api.arn
+    id   = aws_ecr_repository.shop_api.id
+    name = aws_ecr_repository.shop_api.name
+    url  = aws_ecr_repository.shop_api.repository_url
+  }
+}
+
 output "shop_event_projector" {
   description = "ecr repository for shop event projector"
   value = {
