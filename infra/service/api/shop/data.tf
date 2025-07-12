627e87fd5067771a2de4ac1a6228e639b9c9d5f1
tamaco489 <tamaco489@gmail.com>
2025-07-13 00:41:15 +0900

    add: shop api lambda resource

---
+++ infra/service/api/shop/data.tf
@@ -0,0 +1,36 @@
+data "terraform_remote_state" "network" {
+  backend = "s3"
+  config = {
+    bucket = "${var.env}-kinesis-platform-sample-tfstate"
+    key    = "network/terraform.tfstate"
+  }
+}
+
+data "terraform_remote_state" "acm" {
+  backend = "s3"
+  config = {
+    bucket = "${var.env}-kinesis-platform-sample-tfstate"
+    key    = "acm/terraform.tfstate"
+  }
+}
+
+data "terraform_remote_state" "ecr" {
+  backend = "s3"
+  config = {
+    bucket = "${var.env}-kinesis-platform-sample-tfstate"
+    key    = "ecr/terraform.tfstate"
+  }
+}
+
+data "terraform_remote_state" "shop_core_db" {
+  backend = "s3"
+  config = {
+    bucket = "${var.env}-kinesis-platform-sample-tfstate"
+    key    = "credential/core_db/terraform.tfstate"
+  }
+}
+
+# AWS Managed Key
+data "aws_kms_key" "secretsmanager" {
+  key_id = "alias/aws/secretsmanager"
+}
---
+++ infra/service/api/shop/iam.tf
@@ -0,0 +1,69 @@
+# =================================================================
+# basic iam policy
+# =================================================================
+data "aws_iam_policy_document" "lambda_execution_assume_role" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "shop_api" {
+  name               = local.fqn
+  assume_role_policy = data.aws_iam_policy_document.lambda_execution_assume_role.json
+
+  tags = { Name = local.fqn }
+}
+
+# https://docs.aws.amazon.com/ja_jp/aws-managed-policy/latest/reference/AWSLambdaVPCAccessExecutionRole.html
+resource "aws_iam_role_policy_attachment" "shop_api_execution_role" {
+  role       = aws_iam_role.shop_api.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+}
+
+# =================================================================
+# secrets manager iam policy
+# =================================================================
+data "aws_iam_policy_document" "shop_api_secrets_manager" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "secretsmanager:GetSecretValue",
+    ]
+    resources = [data.terraform_remote_state.shop_core_db.outputs.shop_core_db.arn]
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+      "secretsmanager:BatchGetSecretValue",
+    ]
+    resources = ["*"]
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+      "kms:Decrypt",
+    ]
+    resources = [data.aws_kms_key.secretsmanager.arn]
+  }
+}
+
+resource "aws_iam_policy" "shop_api_secrets_manager" {
+  name        = local.fqn
+  description = "Allows Lambda to access Secrets Manager secrets"
+  policy      = data.aws_iam_policy_document.shop_api_secrets_manager.json
+}
+
+resource "aws_iam_role_policy_attachment" "shop_api_secrets_manager" {
+  role       = aws_iam_role.shop_api.name
+  policy_arn = aws_iam_policy.shop_api_secrets_manager.arn
+}
+
+# =================================================================
+# kinesis data stream iam policy
+# =================================================================
+# todo: kinesis data stream に イベントを送信する権限を追加する必要あり
