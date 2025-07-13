5a306d5f3e8325341d303cae73a5a464b874e6a6
tamaco489 <tamaco489@gmail.com>
2025-07-13 15:48:56 +0900

    add: shop api lambda to kinesis data stream vpc endpoint

--- infra/vpc_endpoint/output.tf
+++ infra/vpc_endpoint/output.tf
@@ -2,3 +2,8 @@ output "vpce_secrets_manager_sg" {
   description = "security group for secrets manager vpc endpoint"
   value       = aws_security_group.vpce_secrets_manager.id
 }
+
+output "vpce_kinesis_data_stream_sg" {
+  description = "security group for kinesis data stream vpc endpoint"
+  value       = aws_security_group.vpce_kinesis_data_stream.id
+}
